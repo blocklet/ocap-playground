56c690e5e293fc40955bf4aeb9c78f8c387d68e9
wangshijun <wangshijun2010@gmail.com>
2021-03-30 14:44:17 +0800

    chore: cleanup useless files

--- api/functions/app.js
+++ api/functions/app.js
@@ -1,37 +1,25 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-console */
-// require('../libs/contracts/create_movie_ticket_contract/.compiled/create_movie_ticket/javascript/index');
 const path = require('path');
 const http = require('http');
 const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
-const serverless = require('serverless-http');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fallback = require('express-history-api-fallback');
 const compression = require('compression');
-const EventServer = require('@arcblock/event-server');
 const logger = require('../libs/logger');
 
 const { walletHandlers, walletHandlersWithNoChainInfo, agentHandlers } = require('../libs/auth');
 
-const netlifyPrefix = '/.netlify/functions/app';
 const isProduction = process.env.NODE_ENV === 'production' || !!process.env.BLOCKLET_APP_ID;
-const isNetlify = process.env.NETLIFY && JSON.parse(process.env.NETLIFY);
 
 // Create and config express application
 
 const app = express();
 const server = http.createServer(app);
 logger.initialize(app);
-// Only enable socket server in production, since live reload will also have socket server
-if (isProduction && !isNetlify) {
-  const eventServer = new EventServer(server, ['auth']);
-  walletHandlers.on('scanned', data => data && eventServer.dispatch('auth', data));
-  walletHandlers.on('succeed', data => data && eventServer.dispatch('auth', data));
-  walletHandlers.on('failed', data => data && eventServer.dispatch('auth', data));
-}
 
 app.set('trust proxy', true);
 app.use(cookieParser());
@@ -113,22 +101,17 @@ require('../routes/authorizations').init(router);
 
 if (isProduction) {
   app.use(compression());
-  if (isNetlify) {
-    app.use(netlifyPrefix, router);
-  } else {
-    app.use(router);
-    if (process.env.BLOCKLET_DID) {
-      app.use(`/${process.env.BLOCKLET_DID}`, router);
-    }
-
-    const staticDir = process.env.BLOCKLET_APP_ID ? './' : '../../';
-    const staticDirNew = path.resolve(__dirname, staticDir, 'build');
-    app.use(express.static(staticDirNew, { maxAge: '365d', index: false }));
-    if (process.env.BLOCKLET_DID) {
-      app.use(`/${process.env.BLOCKLET_DID}`, express.static(staticDirNew, { maxAge: '365d', index: false }));
-    }
-    app.use(fallback('index.html', { root: staticDirNew }));
+  app.use(router);
+  if (process.env.BLOCKLET_DID) {
+    app.use(`/${process.env.BLOCKLET_DID}`, router);
   }
+
+  const staticDir = path.resolve(__dirname, '../../', 'build');
+  app.use(express.static(staticDir, { maxAge: '365d', index: false }));
+  if (process.env.BLOCKLET_DID) {
+    app.use(`/${process.env.BLOCKLET_DID}`, express.static(staticDir, { maxAge: '365d', index: false }));
+  }
+  app.use(fallback('index.html', { root: staticDir }));
 
   app.use((req, res) => {
     res.status(404).send('404 NOT FOUND');
@@ -139,12 +122,8 @@ if (isProduction) {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
-} else if (isNetlify) {
-  app.use(netlifyPrefix, router);
 } else {
   app.use(router);
 }
 
-// Make it serverless
-exports.handler = serverless(app);
 exports.server = server;
--- api/require-optional.mock.js
+++
@@ -1,5 +0,0 @@
-module.exports = name => {
-  throw new Error(`optional package does not exist ${name}`);
-};
-
-module.exports.exists = () => false;
