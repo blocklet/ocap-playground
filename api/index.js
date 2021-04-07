2da3deaa60c94747a9fbf6099f1da74b17a58409
wangshijun <wangshijun2010@gmail.com>
2021-04-08 07:46:21 +0800

    fix: api server port

--- api/index.js
+++ api/index.js
@@ -4,12 +4,7 @@ require('dotenv').config();
 const { name, version } = require('../package.json');
 const { server } = require('./functions/app');
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-if (isDevelopment && process.env.ABT_NODE) {
-  process.env.BLOCKLET_PORT = 3030;
-}
-
-const port = parseInt(process.env.BLOCKLET_PORT || process.env.APP_PORT, 10) || 3000;
+const port = parseInt(process.env.BLOCKLET_PORT || 3030, 10);
 server.listen(port, err => {
   if (err) throw err;
   console.log(`> ${name} v${version} ready on ${port}`);
