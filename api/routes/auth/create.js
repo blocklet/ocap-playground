39cc09bae83f68251d5a7a99b3c878acb53fafa5
lipeng <447552708@qq.com>
2022-01-18 21:57:56 +0800

    fix: creat x auth fail (#131)
    Co-authored-by: wangshijun <wangshijun2010@gmail.com>

--- api/routes/auth/create.js
+++ api/routes/auth/create.js
@@ -27,6 +27,7 @@ module.exports = {
           symbol: randomStr('TT'),
           decimal,
           unit: 't',
+          initialSupply: fromTokenToUnit(totalSupply, decimal).toString(),
           totalSupply: fromTokenToUnit(totalSupply, decimal).toString(),
           data: { type: 'json', value: { purpose: 'test' } },
         };
@@ -104,7 +105,7 @@ module.exports = {
   },
 
   // eslint-disable-next-line consistent-return
-  onAuth: async ({ userDid, userPk, claims }) => {
+  onAuth: async ({ userDid, userPk, claims, extraParams: { type: typeUrl } }) => {
     const type = toTypeInfo(userDid);
     const wallet = SDK.Wallet.fromPublicKey(userPk, type);
     const claim = claims.find(x => x.type === 'signature');
@@ -118,19 +119,21 @@ module.exports = {
       tx.from = claim.from;
     }
 
-    if (type === 'token') {
+    if (typeUrl === 'token') {
       const hash = await SDK.sendCreateTokenTx({ tx, wallet });
       return { hash };
     }
 
-    if (type === 'asset') {
+    if (typeUrl === 'asset') {
       const hash = await SDK.sendCreateAssetTx({ tx, wallet });
       return { hash };
     }
 
-    if (type === 'factory') {
+    if (typeUrl === 'factory') {
       const hash = await SDK.sendCreateFactoryTx({ tx, wallet });
       return { hash };
     }
+
+    throw new Error('unsupported current typeUrl');
   },
 };
