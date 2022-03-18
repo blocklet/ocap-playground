bc679f5f64053e812649b4d14e93bc248df03b85
NateRobinson <840501291@qq.com>
2022-03-18 16:44:17 +0800

    suppor sw

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorkerRegistration from './service-worker-registration';
 // eslint-disable-next-line import/no-named-as-default
 import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
