6888b408adb7976ee9077d7ac4d1f84717d889c4
leermao <447552708@qq.com>
2022-03-22 23:40:19 +0800

    chore: update page when servce worker available new content

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+let refreshing = false;
+
 serviceWorkerRegistration.register({
   onUpdate: () => {
-    let refreshing = false;
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      if (refreshing) {
-        return;
-      }
+    if (refreshing) {
+      return;
+    }
 
-      refreshing = true;
-      window.location.reload();
-    });
+    refreshing = true;
+    window.location.reload();
+  },
+  onSuccess: () => {
+    refreshing = false;
   },
 });
