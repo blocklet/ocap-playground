7e790d7e0ae2111849b15ea48a70bfdb94410e17
wangshijun <wangshijun2010@gmail.com>
2022-04-02 20:03:46 +0800

    fix: ci

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorkerRegistration from './service-worker-registration';
 // eslint-disable-next-line import/no-named-as-default
 import App from './app';
 
