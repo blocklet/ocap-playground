739c8c176d42d79bfc356273c4321259c3735418
wangshijun <wangshijun2010@gmail.com>
2022-04-02 18:46:07 +0800

    chore: disable service worker

--- src/index.js
+++ src/index.js
@@ -7,21 +7,3 @@ import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-let refreshing = false;
-
-serviceWorkerRegistration.register({
-  onUpdate: () => {
-    if (refreshing) {
-      return;
-    }
-
-    refreshing = true;
-    window.location.reload();
-  },
-  onSuccess: () => {
-    refreshing = false;
-  },
-});
