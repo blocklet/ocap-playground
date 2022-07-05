24acaf8ed44ee2b498133c0c8392bf856d0cd6d0
NateRobinson <840501291@qq.com>
2022-07-05 14:29:53 +0800

    support some custom menus

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
 require('dotenv').config();
+const Notification = require('@blocklet/sdk/service/notification');
+const { fromTokenToUnit } = require('@ocap/util');
+const { wallet, client, factory: assetFactory } = require('./libs/auth');
+const env = require('./libs/env');
+const itx = require('./libs/token');
+const { ensureAsset } = require('./libs/util');
 
 global.logger = console;
 
@@ -17,4 +23,93 @@ if (isNaN(port)) {
 server.listen(port, err => {
   if (err) throw err;
   console.log(`> ${name} v${version} ready on ${port}`);
+});
+
+Notification.on('menus', async data => {
+  console.log('************');
+  console.log('menus', data);
+  const sender = data.sender.did;
+  await Notification.sendToUser(sender, {
+    type: 'menus',
+    data: {
+      menus: [
+        {
+          name: 'Ask Test Token',
+          type: 'event',
+          data: 'ask-token',
+        },
+        {
+          name: 'Ask Test Asset',
+          type: 'event',
+          data: 'ask-asset',
+        },
+        {
+          name: 'Open ArcBlock.io',
+          type: 'link',
+          data: 'https://www.arcblock.io/',
+        },
+      ],
+    },
+  });
+  console.log('************');
+});
+
+Notification.on('ask-token', async data => {
+  console.log('************');
+  console.log('menus', data);
+  const sender = data.sender.did;
+  try {
+    const amount = Math.random().toFixed(6);
+    await client.transfer({
+      to: sender,
+      token: 0,
+      tokens: [{ address: env.foreignTokenId, value: amount }],
+      wallet,
+    });
+    await Notification.sendToUser(sender, {
+      title: 'Send Random Token',
+      body: 'Some test token has been sent to you, please have a check',
+      attachments: [
+        {
+          type: 'token',
+          data: {
+            address: itx.address,
+            amount: fromTokenToUnit(amount).toString(),
+            symbol: itx.symbol,
+            senderDid: env.appId,
+            chainHost: env.chainHost,
+            decimal: itx.decimal,
+          },
+        },
+      ],
+      actions: [],
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  console.log('************');
+});
+
+Notification.on('ask-asset', async data => {
+  console.log('************');
+  console.log('menus', data);
+  // const sender = data.sender.did;
+  // try {
+  //   const asset = await ensureAsset(assetFactory, {
+  //     userPk: user.pk,
+  //     userDid,
+  //     type: 'badge',
+  //     name: 'Badge',
+  //     description: 'Badge ',
+  //     svg: './public/static/images/badge.svg',
+  //     location: 'China',
+  //     backgroundUrl: '',
+  //     logoUrl: 'https://releases.arcblockio.cn/arcblock-logo.png',
+  //     startTime: new Date(),
+  //     endTime: new Date(Date.now() + 2 * 60 * 60 * 1000),
+  //   });
+  // } catch (e) {
+  //   console.error(e);
+  // }
+  console.log('************');
 });
