d5247fd9f9d07201dc1e23b068206c2e417bbdd6
wangshijun <wangshijun2010@gmail.com>
2022-09-06 16:45:17 +0800

    fix: connect-only case is broken #194

--- api/routes/auth/claim-connect-only.js
+++ api/routes/auth/claim-connect-only.js
@@ -1,5 +1,12 @@
 module.exports = {
   action: 'connect-only',
+  authPrincipal: false,
+  claims: {
+    authPrincipal: {
+      description: 'Connect your DID Wallet to continue',
+      supervised: true,
+    },
+  },
   onAuth: async ({ userDid }) => {
     return { successMessage: `You are connected ${userDid}` };
   },
