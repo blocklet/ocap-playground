25a0d914cec6523690b31ab6d49efc4b1c29006a
wangshijun <wangshijun2010@gmail.com>
2023-02-09 16:50:34 +0800

    chore: add routes for derive/delegate/encryptionKey

---
+++ api/routes/auth/derive-key-pair.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+const { types, Hasher } = require('@ocap/mcrypto');
+const AES = require('@ocap/mcrypto/lib/crypter/aes').default;
+const { fromSecretKey } = require('@ocap/wallet');
+const { fromBase58, toBase58 } = require('@ocap/util');
+
+const { User } = require('../../models');
+
+module.exports = {
+  action: 'derive-key-pair',
+  claims: {
+    keyPair: () => {
+      return {
+        mfa: true,
+        description: 'Please generate a new key-pair',
+        targetType: {
+          role: 'application',
+          hash: 'sha3',
+          key: 'ed25519',
+          encoding: 'base58',
+        },
+      };
+    },
+  },
+
+  onAuth: async ({ claims, extraParams: { sessionDid } }) => {
+    const claim = claims.find(x => x.type === 'keyPair');
+
+    const app = fromSecretKey(fromBase58(claim.secret), {
+      role: types.RoleType.ROLE_APPLICATION,
+    });
+
+    const password = Hasher.SHA3.hash256(Buffer.concat([claim.secret, app.address].map(fromBase58)));
+    const user = await User.ensureOne({ did: sessionDid });
+
+    user.generatedApps = [
+      { address: app.address, publicKey: toBase58(app.publicKey), encrypted: AES.encrypt('playground', password) },
+    ];
+    await User.update(user);
+
+    return {
+      successMessage: `You have generated new app did: ${app.address}`,
+    };
+  },
+};
---
+++ api/routes/auth/sign-delegation.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-console */
+const assert = require('assert');
+const { toBase64, fromBase58, toHex } = require('@ocap/util');
+const { sign, verify } = require('@arcblock/jwt');
+
+const { User } = require('../../models');
+const { wallet } = require('../../libs/auth');
+
+module.exports = {
+  action: 'sign-delegation',
+  authPrincipal: false,
+  claims: [
+    {
+      authPrincipal: async ({ extraParams: { sessionDid } }) => {
+        const user = await User.ensureOne({ did: sessionDid });
+        const [app] = user.generatedApps;
+        assert(app, 'You must generate an application account first');
+
+        return {
+          description: 'Please select application account',
+          target: app.address,
+        };
+      },
+    },
+    {
+      signature: async ({ userDid, userPk, extraParams: { sessionDid } }) => {
+        const user = await User.ensureOne({ did: sessionDid });
+        const [app] = user.generatedApps;
+        assert(app, 'You must generate an application account first');
+
+        // 生成token的header和payload
+        const headerAndPayload = sign(
+          userDid,
+          undefined,
+          {
+            from: app.address,
+            to: wallet.address,
+            userPk,
+            permissions: [{ role: 'DIDConnectAgent', claims: ['encryptionKey'] }],
+            exp: new Date().getTime() / 1000 + 60 * 60,
+            version: '1.1.0',
+          },
+          false
+        );
+
+        const data = toHex(headerAndPayload);
+
+        return {
+          meta: {
+            headerAndPayload,
+          },
+          description: 'Sign this delegation to allow playground to retrieve encryption key',
+          mfa: true,
+          data,
+        };
+      },
+    },
+  ],
+
+  onAuth: async ({ claims, extraParams: { sessionDid } }) => {
+    const user = await User.ensureOne({ did: sessionDid });
+    const [app] = user.generatedApps;
+    assert(app, 'You must generate an application account first');
+
+    const claim = claims.find(c => c.type === 'signature');
+    const delegation = `${claim.meta.headerAndPayload}.${toBase64(fromBase58(claim.sig))}`;
+    assert(verify(delegation, fromBase58(app.publicKey)), 'delegation verify failed');
+
+    app.delegation = delegation;
+
+    user.generatedApps = [app];
+    await User.update(user);
+
+    return {
+      successMessage: 'You have signed delegation for playground',
+    };
+  },
+};
